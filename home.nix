4772a9af96e342878190882705abdaa3b725144d
John Jorgensen <jjorgensen@hsdp.io>
2026-06-04 11:49:07 -0400

    line up emacs packages with .emacs.d

--- home.nix
+++ home.nix
@@ -69,15 +69,18 @@ in {
       epkgs.org-tree-slide
       epkgs.platformio-mode
       epkgs.popup
+      epkgs.pos-tip
       epkgs.projectile
       epkgs.rust-mode
+      epkgs.swiper
       epkgs.toml-mode
       epkgs.transient
       epkgs.vterm
+      epkgs.web-mode
+      epkgs.with-editor
       epkgs.yaml-mode
       epkgs.yasnippet
       epkgs.yasnippet-snippets
-      epkgs.web-mode
     ]))
   ];
 
